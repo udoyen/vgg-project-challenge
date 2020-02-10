f31a16b1577420dbe7bc5b62a2314d2228891eb7
udoyen <datameshprojects@gmail.com>
2020-02-10 21:18:58 +0100

    removed docker files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 def create_app(config_filename):
     app = Flask(__name__)
     app.config.from_object(config_filename)
@@ -15,4 +14,4 @@ def create_app(config_filename):
 
 if __name__ == "__main__":
     app = create_app("config")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True, port=80)
